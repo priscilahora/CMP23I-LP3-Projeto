a10bcb8684031a00d5263aefa7031a0afb43a2ae
priscilahora <priscilaphora@gmail.com>
2023-06-27 21:33:32 -0300

    alterando projeto

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,47 +1,31 @@
 function somar() {
   const numero1 = parseFloat(document.getElementById("numero1Soma").value);
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
-  // evite mexer no código acima!
-
+ 
   const soma = numero1 + numero2;
-
-  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
-  Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
   document.getElementById("resultadoSoma").innerHTML = soma;
 }
 
 function subtrair() {
   const numero1 = parseFloat(document.getElementById("numero1Sub").value);
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
-  // evite mexer no código acima!
 
   const subtracao = numero1 - numero2;
-
-  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
-   Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
   document.getElementById("resultadoSub").innerHTML = subtracao;
 }
 
 function multiplicar() {
   const numero1 = parseFloat(document.getElementById("numero1Mult").value);
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
-  // evite mexer no código acima!
 
-  const multiplicacao = numero1 * numero2;
-
-  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
-   Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
-  document.getElementById("resultadoMult").innerHTML = multiplicacao;
+  const produto = numero1 * numero2;
+  document.getElementById("resultadoMult").innerHTML = produto;
 }
 
 function dividir() {
   const numero1 = parseFloat(document.getElementById("numero1Div").value);
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
-  // evite mexer no código acima!
 
-  const divisao = numero1 / numero2;
-
-  /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
-   Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
+  const divisao = numero1/numero2;
   document.getElementById("resultadoDiv").innerHTML = divisao;
 }
